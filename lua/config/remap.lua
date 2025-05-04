84a70ec23e913dccb7dae193f1bcf34da9fbceed
chu pack <91909231+DaneX3089@users.noreply.github.com>
2025-05-04 21:15:45 +0400

    haa

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -5,6 +5,11 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
+vim.keymap.set('i', '<C-j>', '<Down>')
+vim.keymap.set('i', '<C-k>', '<Up>')
+vim.keymap.set('i', '<C-l>', '<Right>')
+vim.keymap.set('i', '<C-h>', '<Left>')
+
 
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
